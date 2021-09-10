b6b3ebfbc5f2d2b709723be35a9babf8887b0fef
AdiEllis <adi.ellis3@gmail.com>
2021-09-10 13:26:41 +0300

    added DateValidator

---
+++ src/main/java/com/project/Utils/DateValidator.java
@@ -0,0 +1,25 @@
+package com.project.Utils;
+
+import org.springframework.stereotype.Component;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+@Component
+public class DateValidator {
+
+    public boolean isValid(String dateStr) {
+        DateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setLenient(false);
+        try {
+            sdf.parse(dateStr);
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
+}
